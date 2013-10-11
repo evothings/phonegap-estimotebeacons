c064eac65574ad5bb81a88e35a318424b54cbd18
Konrad Dzwinel <kdzwinel@gmail.com>
2013-10-11 14:32:49 +0200

    Fixing MyPlugin.js

--- www/MyPlugin.js
+++ www/MyPlugin.js
@@ -4,19 +4,16 @@ var exec = require('cordova/exec');
  */
 function MyPlugin() {}
 
-MyPlugin.prototype.echo = function() {
-  exec(function(result){
-      // result handler
-      alert(result);
-    },
-    function(error){
-      // error handler
-      alert("Error" + error);
-    }, 
-    "MyPlugin", 
-    "echo",
-    []
-  );
+MyPlugin.prototype.echo = function(text, callback) {
+    exec(callback,
+        function(error){
+            // error handler
+            console.error("Error", error);
+        },
+        "MyPlugin",
+        "echo",
+        [text]
+    );
 };
 
 var myPlugin = new MyPlugin();
